2d3db0cfe09c63f622a3b6975cf4aac574019685
pammy0318 <pamellemadulara18@gmail.com>
2025-07-07 17:00:22 +0800

    UseFaci
    Added Commercial.vue file
    Added Rental.vue file
    Edited Hostel.vue
    Edited uf_home.bue
    Edited web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,23 @@ Route::get('/', function () {
     return Inertia::render('LandingPage');
 })->name('home');
 
+Route::get('/use-of-facilities', function () {
+    return Inertia::render('UseFaci/uf_home');
+});
+
+Route::get('/use-of-facilities/hostel', function () {
+    return Inertia::render('UseFaci/Hostel');
+})->name('usefacilities.hostel');
+
+Route::get('/use-of-facilities/commercial', function () {
+    return Inertia::render('UseFaci/Commercial'); 
+})->name('usefacilities.commercial');
+
+Route::get('/use-of-facilities/rental', function () {
+    return Inertia::render('UseFaci/Rental'); 
+})->name('usefacilities.rental');
+
+
 // Route::get('dashboard', function () {
 //     return Inertia::render('Dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
