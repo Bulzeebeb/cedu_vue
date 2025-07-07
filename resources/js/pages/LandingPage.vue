41a595dc4a553582ab20f52e306fec68c46ab607
bulzeebeb <jpcosorio00256@usep.edu.ph>
2025-07-07 13:42:56 +0800

    landing pagev3

--- resources/js/pages/LandingPage.vue
+++ resources/js/pages/LandingPage.vue
@@ -3,12 +3,8 @@
     <!-- Header -->
     <header class="bg-maroon text-white py-2 px-4 flex justify-between items-center">
       <div class="flex items-center gap-2">
-<<<<<<< Updated upstream
-        <img src="/logo.png" alt="CEDU Logo" class="h-10 w-10" />
-        <h1 class="text-lg font-bold text-white">CEDU <span class="text-blue-300">i<span class="text-lg font-bold text-white">Central</span></span></h1>
-=======
+        <img src="/images/logo.png" alt="CEDU Logo" class="h-10 w-10" />
         <h1 class="text-lg font-bold text-white">CEDU  <span class="text-yellow-300">iCentral</span></h1>
->>>>>>> Stashed changes
       </div>
       <nav class="flex gap-2 ">
         <button class="nav-btn">Pay-to-Park</button>
@@ -54,7 +50,7 @@
       <div>
         <h3 class="font-bold mb-1">Support</h3>
         <p>University of Southeastern Philippines<br>Tagum-Mabini Campus<br>Apokon, Tagum City</p>
-        <p>osorio.jg@usep.edu.ph<br>+63915-8538-959</p>
+        <p>cedu@usep.edu.ph<br>+63915-8538-959</p>
       </div>
       <div>
         <h3 class="font-bold mb-1">Account</h3>
@@ -93,10 +89,16 @@ export default {
   background-color: #650000;
 }
 .nav-btn {
-  background: white;
-  color: black;
+  color: white;
   padding: 6px 12px;
   border-radius: 4px;
   font-weight: 500;
+  transition: color 0.3s ease;
 }
+
+.nav-btn:hover {
+  color: #ffd700;
+}
+
+
 </style>
